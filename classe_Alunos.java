31b04b6e520e53f800ad47b0ec094dd39722fadd
ALINE RAMOS DE OLIVEIRA <137660700+AlineRDO@users.noreply.github.com>
2024-09-12 18:24:14 -0300

    Add files via upload

---
+++ classe_Alunos.java
@@ -0,0 +1,57 @@
+package com.escolinha.saudealunos.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String dataNascimento;
+    private String endereco;
+    private String contato;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
